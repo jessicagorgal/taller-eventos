85d92ac4531b8c3a1e7ea44fb06a6fb9673b4a7e
jessicagorgal <jessicagorgal@gmail.com>
2024-09-08 21:15:52 -0300

    terminado
    Co-Authored-By: jessicagorgal <137963474+jessicagorgal@users.noreply.github.com>

---
+++ eventos.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector("div");
+
+btn.addEventListener("click", () => {
+    alert("¡Hola! Soy el div ");
+    
+});
+
+function clicked() {
+    alert("¡Hola!");
+    
+}
